d1e476474a54a0196aff15a0d49ef9e7db014630
Ali Kingravi <alikingravi@hotmail.com>
2018-04-09 16:40:02 +0000

    Added doc blocks to controller

--- app/controllers/DataController.php
+++ app/controllers/DataController.php
@@ -1,17 +1,28 @@
 <?php
 
+/**
+ * Class DataController
+ */
 class DataController
 {
+    /**
+     * Index function is run as the entry method
+     */
     public static function index()
     {
-        // Find the file
+        // Path to all batch files
         $directory = __DIR__ . "/../../public/batchData";
 
+        // File Path Finder
         $files = (new FilePathFinder())->findFiles($directory);
 
+        // Path to appCodes.ini
         $appCodesPath = __DIR__ . "/../../public/batchData/appCodes.ini";
+
+        // Csv Data Transformer
         $csvDataTransformer = new CsvDataTransformer(new AppCodes($appCodesPath));
 
+        // Write data to new csv file
         foreach ($files as $file) {
             try {
                 $fileParser = new FileParser();
